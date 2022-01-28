e573060fe93683f0f9315543db5bb46bb017b734
rahulmlokurte <rahul.m.lokurte@gmail.com>
2022-01-28 15:08:56 +0530

    fix the sudo

--- kubernetes/kubectl.sh
+++ kubernetes/kubectl.sh
@@ -7,7 +7,5 @@ kubectl_version="1.23.3"
 echo "Downloading kubectl..."
 curl -LO https://dl.k8s.io/release/v${kubectl_version}/bin/linux/amd64/kubectl
 chmod +x ./kubectl
-mkdir -p ~/.local/bin/kubectl
-mv ./kubectl ~/.local/bin/kubectl
-export PATH="$HOME/.local/bin/kubectl:$PATH"
-kubectl version --client
+sudo mv ./kubectl /usr/local/bin/kubectl
+echo "KUBECTL_VERSION=$(kubectl version --client)"
