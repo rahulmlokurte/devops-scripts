a498a1347b8ca1cd8453aa3a50ec2a654271543d
rahulmlokurte <rahul.m.lokurte@gmail.com>
2022-02-04 23:38:06 +0530

    Adding python script for helm

--- kubernetes.py
+++ kubernetes.py
@@ -3,8 +3,11 @@ from pathlib import Path
 
 eksctl = Path('./kubernetes/eksctl.sh')
 kubectl = Path('./kubernetes/kubectl.sh')
+helm = Path('./kubernetes/helm.sh')
 eksctl.chmod(0o755)
 kubectl.chmod(0o755)
+helm.chmod(0o755)
 
 subprocess.run("cd ./kubernetes && ./eksctl.sh", shell=True)
 subprocess.run("cd ./kubernetes && ./kubectl.sh ", shell=True)
+subprocess.run("cd ./kubernetes && ./helm.sh ", shell=True)
